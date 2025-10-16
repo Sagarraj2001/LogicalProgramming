661899a0b00d9fa42af92e60aaa4a9203ac2d181
Sagarraj2001 <sagarraj19042001@gmail.com>
2025-10-16 22:24:36 +0530

    Pattern Folder has been updated

---
+++ Pattern/src/PatternSet7/Pattern13.java
@@ -0,0 +1,24 @@
+package PatternSet7;
+
+public class Pattern13 {
+	public static void main(String[] args) {
+		String s="hello";
+		int sp=s.length()*2-3;
+		for(int i=0;i<=s.length()-1;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(s.charAt(j));
+			}
+			for(int k=1;k<=sp;k++) {
+				System.out.print(" ");
+			}
+			for(int j=i;j>=0;j--) {
+				if(j==i && i==s.length()-1) {
+					continue;
+				}
+				System.out.print(s.charAt(j));
+			}
+			sp-=2;
+			System.out.println();
+		}
+	}
+}
